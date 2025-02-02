d25217ebe120636013db42a8cc2ba8c3b2362afa
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-02 20:00:48 -0300

    Inserido todos os botões da calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text, View } from "react-native"
 import Button from "./components/Button"
 
 function App() {
-    return(
-        <SafeAreaView style={style.App}>
-            <Text>Calculadora</Text>
-            <Button label="CA"/>
+    return (
+        <SafeAreaView>
+            <View style={style.container}>
+                <View style={style.buttons}>
+                    <Button label="AC" />
+                    <Button label="/" />
+                    <Button label="7" />
+                    <Button label="8" />
+                    <Button label="9" />
+                    <Button label="*" />
+                    <Button label="4" />
+                    <Button label="5" />
+                    <Button label="6" />
+                    <Button label="-" />
+                    <Button label="1" />
+                    <Button label="2" />
+                    <Button label="3" />
+                    <Button label="+" />
+                    <Button label="0" />
+                    <Button label="." />
+                    <Button label="=" />
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
 
 const style = StyleSheet.create({
-    App: {
-        backgroundColor:"#fff",
+   
+    container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems:"center",
-        padding:20
+    },
+    buttons:{
+        flexDirection:"row",
+        flexWrap:"wrap"
     }
+
+
 })
 
 export default App
