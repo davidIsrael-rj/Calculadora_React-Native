2752b21d9a74399ee418c4e9758be3dbb880d410
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-02 20:24:29 -0300

    Display

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,47 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native"
+import React, { Component } from "react"
+import { StyleSheet, View } from "react-native"
 import Button from "./components/Button"
+import Display from "./components/Display"
 
-function App() {
-    return (
-        <SafeAreaView>
-            <View style={style.container}>
-                <View style={style.buttons}>
-                    <Button label="AC" />
-                    <Button label="/" />
-                    <Button label="7" />
-                    <Button label="8" />
-                    <Button label="9" />
-                    <Button label="*" />
-                    <Button label="4" />
-                    <Button label="5" />
-                    <Button label="6" />
-                    <Button label="-" />
-                    <Button label="1" />
-                    <Button label="2" />
-                    <Button label="3" />
-                    <Button label="+" />
-                    <Button label="0" />
-                    <Button label="." />
-                    <Button label="=" />
+export default class App extends Component {
+    state ={
+        displayValue: '0'
+    }
+    render() {
+        return (
+                <View style={styles.container}>
+                        <Display value={this.state.displayValue}/>
+                    <View style={styles.buttons}>
+                        <Button label="AC" />
+                        <Button label="/" />
+                        <Button label="7" />
+                        <Button label="8" />
+                        <Button label="9" />
+                        <Button label="*" />
+                        <Button label="4" />
+                        <Button label="5" />
+                        <Button label="6" />
+                        <Button label="-" />
+                        <Button label="1" />
+                        <Button label="2" />
+                        <Button label="3" />
+                        <Button label="+" />
+                        <Button label="0" />
+                        <Button label="." />
+                        <Button label="=" />
+                    </View>
                 </View>
-            </View>
-        </SafeAreaView>
-    )
+        )
+    }
 }
 
-const style = StyleSheet.create({
-   
+const styles = StyleSheet.create({
     container: {
-        flex: 1,
+      flex: 1,
     },
-    buttons:{
-        flexDirection:"row",
-        flexWrap:"wrap"
+    buttons: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
     }
-
-
-})
-
-export default App
+  });
+  
