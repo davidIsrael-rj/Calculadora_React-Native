0fcbb71535b991c32f79e16b99a9ce5d9f175777
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-03 09:21:05 -0300

    Adicionando estilo aos botões

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,23 @@ export default class App extends Component {
                 <View style={styles.container}>
                         <Display value={this.state.displayValue}/>
                     <View style={styles.buttons}>
-                        <Button label="AC" />
-                        <Button label="/" />
+                        <Button label="AC" operation triple />
+                        <Button label="/" operation/>
                         <Button label="7" />
                         <Button label="8" />
                         <Button label="9" />
-                        <Button label="*" />
+                        <Button label="*" operation/>
                         <Button label="4" />
                         <Button label="5" />
                         <Button label="6" />
-                        <Button label="-" />
+                        <Button label="-" operation/>
                         <Button label="1" />
                         <Button label="2" />
                         <Button label="3" />
-                        <Button label="+" />
-                        <Button label="0" />
+                        <Button label="+" operation/>
+                        <Button label="0" double/>
                         <Button label="." />
-                        <Button label="=" />
+                        <Button label="=" operation/>
                     </View>
                 </View>
         )
